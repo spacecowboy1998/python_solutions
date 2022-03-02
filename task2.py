04767b342ecb71aab5d76bd29602ef66caabf010
akaki <akakibenidze17@gmail.com>
2022-03-02 18:41:35 +0400

    hello

--- task2.py
+++ task2.py
@@ -5,6 +5,10 @@ class Employee:
         self.asaki = age
         self.shemosavali = salary
 
+    def Print_info(self):
+        print(f"დასაქამებულის მონაცებემი სახელი:{self.saxeli}, გვარი:{self.gvari},"
+              f" შემოსავალი:{self.shemosavali} ლარი , ასაკი:{self.asaki} წელი")
+
 
 my_file = open("dataset1.csv", "r", encoding="utf8")
 my_list = my_file.readlines()
@@ -24,8 +28,11 @@ for person in my_list:
 min_salary_person = min(obj_list, key=lambda x: x.shemosavali)
 max_age_person = max(obj_list, key=lambda x: x.asaki)
 
-print(f"მინიმალური ხელფასი არის {min_salary_person.shemosavali} ლარი,იგი ეკუთვნის "
-      f"{min_salary_person.saxeli} {min_salary_person.gvari}ს და მისი ასაკია {min_salary_person.asaki} წელი")
+min_salary_person.Print_info()
+max_age_person.Print_info()
 
-print(f"ყველაზე დიდი ასაკი არის {max_age_person.asaki} წელი ,იგი ეკუთვნის "
-      f"{max_age_person.saxeli} {max_age_person.gvari}ს და მისი შემოსავალია {max_age_person.asaki} ლარი")
+# print(f"მინიმალური ხელფასი არის {min_salary_person.shemosavali} ლარი,იგი ეკუთვნის "
+#       f"{min_salary_person.saxeli} {min_salary_person.gvari}ს და მისი ასაკია {min_salary_person.asaki} წელი")
+#
+# print(f"ყველაზე დიდი ასაკი არის {max_age_person.asaki} წელი ,იგი ეკუთვნის "
+#       f"{max_age_person.saxeli} {max_age_person.gvari}ს და მისი შემოსავალია {max_age_person.asaki} ლარი")
